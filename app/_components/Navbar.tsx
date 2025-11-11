388d425571f26acd592837be0744e6e378725970
preetivithal2 <saniyapreeti3@gmail.com>
2025-11-12 04:35:18 +0500

    change navbar color

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -14,7 +14,7 @@ function classNames(...classes:any) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="relative bg-gray-800">
+    <Disclosure as="nav" className="relative bg-[#F4EFEC]">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
