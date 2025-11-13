e4b037ee35116981fe83881fef2b7eee69b778f8
preetivithal2 <saniyapreeti3@gmail.com>
2025-11-13 06:38:00 +0500

    correct coffee banner

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -17,18 +17,46 @@ function Banner() {
                 </div>
             </div>*/}
 
+<section className="w-full bg-[#f5ede7] flex flex-col items-center justify-center relative">
+  {/* Coffee Image */}
+  <img
+    src="/Coffee.png"
+    alt="coffee"
+    className="w-full object-cover h-[500px] md:h-[600px] lg:h-[700px]"
+  />
 
-            <section className="w-full bg-[#f5ede7] flex flex-col items-center">
+  {/* Text Content */}
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-5">
+    <h1 className="text-[#461901] font-bold text-4xl md:text-6xl mb-4">Coffee</h1>
+    <p className="text-black max-w-2xl text-sm md:text-base">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus voluptas qui aliquam saepe? Dolorem eligendi cum voluptatem.
+    </p>
+
+    {/* Buttons */}
+    <div className="flex flex-col md:flex-row gap-4 mt-6">
+      <button className="bg-[#461901] text-white rounded-2xl px-8 py-2 hover:bg-[#5c2402] transition">
+        Order Now
+      </button>
+      <button className="bg-[#461901] text-white rounded-2xl px-8 py-2 hover:bg-[#5c2402] transition">
+        Best Deal
+      </button>
+    </div>
+  </div>
+</section>
+
+            {/* <section className="w-full bg-[#f5ede7] flex flex-col items-center">
                 <img src="/Coffee.png" alt="coffee" className="w-full object-cover" />
-                <h2 className="text-3xl font-bold text-brown-800 mt-4">Coffee</h2>
-                <p className="text-center max-w-2xl">Lorem ipsum dolor sit amet...</p>
+                <div className='flex items-center justify-center flex-col relative w-100 sm:not-[relative]:'>
+                    <h1 className='text-[#461901] font-bold text-6xl absolute bottom-[300px]'>CoFFee</h1>
+                    <p className='text-black flex text-center items-center justify-content-center mx-5 absolute bottom-[220px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. temporibus voluptas qui aliquam saepe? Dolorem eligendi cum voluptatem.</p>
+                </div>
             </section>
 
 
-            <div className='flex flex-col w-[300px] '>
-                <button className='bg-[#461901] color-white rounded-2xl px-6 py-2 m-2 '>Order Now</button>
-                <button className='bg-[#461901] color-white rounded-2xl px-6 py-2 m-2'>Best deal</button>
-            </div>
+            <div className='flex flex-col w-[300px] relative '>
+                <button className='bg-[#461901] color-white rounded-2xl px-[100px] py-2 m-2 absolute bottom-[150px]'>Order Now</button>
+                <button className='bg-[#461901] color-white rounded-2xl px-[103px] py-2 m-2 absolute bottom-[100px]'>Best deal</button>
+            </div> */}
         </div>
     )
 
