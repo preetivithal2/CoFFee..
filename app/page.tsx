65fbb7a5e8815dfdf6141301c5f3e22e65ff4e59
preetivithal2 <saniyapreeti3@gmail.com>
2025-11-20 01:13:46 +0500

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import Navbar from "./_components/Navbar";
 import Banner from "./_components/Banner";
 import Services from "./_components/Services";
 import Offer from "./_components/Offer";
+import Footer from "./_components/Footer";
+
+
 
 export default function Home() {
   return (
     <div>
-
       <Navbar />
       <Banner />
       <Services />
       <Offer />
-    </div>
+      <Footer />
+      
+          </div>
   );
 }
