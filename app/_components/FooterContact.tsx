4030805ffad425fac8c426d4c0d943942ed37ac9
preetivithal2 <saniyapreeti3@gmail.com>
2025-11-22 01:31:44 +0500

    edit footer 2

--- app/_components/FooterContact.tsx
+++ app/_components/FooterContact.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function ContactFooter() {
   return (
-    <footer className="w-full bg-[#E8DDD3] py-12 px-4 md:px-12 lg:px-20 ">
+    <footer className="w-full bg-[#E8DDD3] pt-12 px-4 md:px-12 lg:px-20 ">
       <div className=" grid grid-cols-1 lg:grid-cols-2 gap-10 items-center mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         {/* Left Content */}
         <div className="space-y-6">
@@ -58,68 +58,21 @@ export default function ContactFooter() {
             SUBMIT
           </button>
         </form>
-        {/* <div className="flex items-center flex-col justify-center"><hr className="my-6 border-default sm:mx-auto lg:my-8" />
-        <span className="block text-sm text-body text-amber-900 sm:text-center ">© 2025 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span></div> */}
-    
-
-
-   <footer className="bg-neutral-primary-soft rounded-base shadow-2xl border border-default m-4 ">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        
-        <div className="sm:flex sm:items-center sm:justify-between text-black">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-7"
-              alt="Flowbite Logo"
-            />
-            <span className="text-heading self-center text-2xl font-semibold whitespace-nowrap">
-              Flowbite
-            </span>
-          </a>
-
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0">
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <hr className="my-6 border-amber-950 sm:mx-auto lg:my-8" />
-
-        <span className="block text-sm text-body text-center ">
+      </div>
+      <div className="m-7 w-full">
+        <p className="text-black text-center">
           © 2025{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
+          <a href="https://AIORAA.com/" className="hover:underline">
+            AIORAA™
           </a>
           . All Rights Reserved.
+        </p>
+        <span className="block text-sm text-body text-center ">
+
         </span>
       </div>
-    </footer>
 
 
-
-      </div>
     </footer>
   );
 }
